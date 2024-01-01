a304e0bc14ae901d8acee6654b270cff349b9064
soray677 <soray677@outlook.com>
2024-01-01 23:29:38 +0900

    Updates

---
+++ assets/Configure-vEicamaq.js
@@ -0,0 +1,8 @@
+import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{M as s}from"./index-jjTjbubn.js";import{useMDXComponents as r}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-RWjO5u2k.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";function o(n){const e=Object.assign({p:"p"},r(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(s,{title:"introduction"}),`
+`,t.jsxs("main",{children:[t.jsx("h1",{children:"俺の考えたコンポーネント図鑑"}),t.jsx("p",{children:"Reactによるコンポーネントを作成し、パーツとして公開しています。"}),t.jsx("p",{children:t.jsxs(e.p,{children:["各docsをお読みいただいた上で、利用したいものをご自由にお使いください。",t.jsx("br",{}),`
+※ 中にはSoraY677自身で考えたものではなく、参照を元に作成したものもあります。必ず説明を読んで自己責任でご使用ください。`]})})]}),`
+`,t.jsx("style",{children:`
+  main {
+    color: #2e2e2e;
+  }
+  `})]})}function f(n={}){const{wrapper:e}=Object.assign({},r(),n.components);return e?t.jsx(e,Object.assign({},n,{children:t.jsx(o,n)})):o(n)}export{f as default};
