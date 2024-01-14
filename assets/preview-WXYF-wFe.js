b31a7f6c10578a0f8ff28de4642286ad88558c09
soray677 <soray677@outlook.com>
2024-01-14 12:23:56 +0900

    Updates

---
+++ assets/preview-WXYF-wFe.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-NaN1tpUS.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-4w5tAQ8Z.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-4w5tAQ8Z.js","./iframe-NaN1tpUS.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-BPx8uaqq.js","./extends-cGC6D0nu.js","./index-ogXoivrg.js","./inheritsLoose-N4InywQs.js","./index-MVbLLYTZ.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
